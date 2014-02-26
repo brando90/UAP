fix some bugs. The mean, std and the amount where wrong order for numpy and self.field was used properly

generate_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class DataGenerator:
-	def __init__(self, amount, number_of_groups, mean, std):
+	def __init__(self, number_of_groups, amount, mean, std):
 		self.amount = amount
 		self.number_of_groups = number_of_groups
 		self.mean = mean
@@ -10,16 +10,16 @@ class DataGenerator:
 	def GenerateData(self):
 		g_data = []
 		for group_index in range(number_of_groups):
-			data = self.generateDataPoint(amount, mean, std)
+			data = self.generateDataPoint(self.amount, self.mean, self.std)
 			g_data.append(data)
 		return g_data
 
 	def generateDataPoint(self, amount, mean, std):
-		return np.random.normal(amount, mean, std)
+		return np.random.normal(mean, std, amount)
 
 number_of_groups = 10
 amount = 3
 mean = 1
 std = 2
-gen = DataGenerator(number_of_groups,amount,mean,std)
+gen = DataGenerator(number_of_groups, amount, mean, std)
 print gen.GenerateData()